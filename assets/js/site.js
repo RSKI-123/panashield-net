317b3a5c01dfbd261e9a0804bfcf46f14c606812
RSKI-123 <admin@risukai.co.jp>
2026-05-22 15:02:24 +0900

    Add Lukurasu iPad case products

--- assets/js/site.js
+++ assets/js/site.js
@@ -21,7 +21,7 @@ const checkoutSummary = document.querySelector("[data-checkout-summary]");
 const checkoutStatus = document.querySelector("[data-checkout-status]");
 const productsSection = document.getElementById("products");
 const VARIANT_CHIP_LIMIT = 36;
-const PRODUCTS_DATA_VERSION = "20260516211900";
+const PRODUCTS_DATA_VERSION = "2026052201";
 const PRODUCTS_DATA_URL = `assets/data/products.json?v=${PRODUCTS_DATA_VERSION}`;
 
 const currencyFormatter = new Intl.NumberFormat("ja-JP", {
